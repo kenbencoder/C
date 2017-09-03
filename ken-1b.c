7313cca9f6376ec464b6cdd828646cc1c7e37f2b
Kenneth Hsieh <KH@Kenneth-Hsiehs-MacBook-Pro.local>
2017-09-03 23:13:36 +1000

    Lesson 1 practice files.

---
+++ ken-1b.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+int main()
+{
+printf("hello ken\n");
+printf("hello ben");
+
+// printf(10);
+
+return 0;
+}
+
---
+++ ken-1c.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+
+int main() {
+//	printf("%d\n",1);
+//	printf("%f\n",3.147182081);
+	printf("%f\n%s\n%d\n",3.142,"hello world",1);
+return 0;
+}
